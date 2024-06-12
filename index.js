5625b4bf31f724bb4f29adc9ea01b36199482e84
lijoyoommen007 <108475172+lijoyoommen007@users.noreply.github.com>
2024-06-12 16:23:39 +0530

    added user role to creat user

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const express = require('express');
 const flash = require('express-flash');
 const bodyParser = require('body-parser');
 const session = require('express-session');
-const SequelizeStore = require("connect-session-sequelize")(session.Store); // initalize sequelize with session store
+const SequelizeStore = require("connect-session-sequelize")(session.Store); // initialize sequelize with session store
 
 const app = express();
 const router = express.Router();
 
-//Loading Routes 
+// Load Routes 
 const webRoutes = require('./routes/web');
 const sequelize = require('./config/database');
 const errorController = require('./app/controllers/ErrorController');
@@ -19,18 +19,18 @@ const errorController = require('./app/controllers/ErrorController');
 env.config();
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
-
 app.use(webRoutes);
 app.use(errorController.pageNotFound);
 
+// Sync the database and alter tables to match models
 sequelize
-    .sync()
+    .sync({ alter: true })
     .then(() => {
         app.listen(process.env.PORT, '0.0.0.0'); // Listen on IP address 0.0.0.0
-        //pending set timezone
         console.log("App listening on port " + process.env.PORT);
     })
     .catch(err => {
         console.log(err);
     });
+
+module.exports = app;
