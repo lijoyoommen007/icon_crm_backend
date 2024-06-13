c606572f030ca4b933882d1a79feb3e3fcfd65a7
lijoyoommen007 <108475172+lijoyoommen007@users.noreply.github.com>
2024-06-13 12:35:58 +0530

    user data added in auth

--- app/controllers/AuthController.js
+++ app/controllers/AuthController.js
@@ -4,37 +4,48 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User'); // Adjust the path as necessary
 
 const authenticateUser = async (req, res) => {
-  const { username, password } = req.body;
-  if (!username || !password) {
-    return res.status(401).json({ message: "No email or password" });
-  }
- 
-  try {
-    // Find the user by email
-    const user = await User.findOne({ where: { username: username } });
+	const { username, password } = req.body;
+	if (!username || !password) {
+	  return res.status(401).json({ message: "No email or password" });
+	}
+   
+	try {
+	  // Find the user by email
+	  const user = await User.findOne({ where: { username: username } });
+  
+	  if (!user) {
+		return res.status(404).json({ message: 'User not found' });
+	  }
+  
+	  // Compare the provided password with the stored hash
+	  const isMatch = await bcrypt.compare(password, user.password);
+  
+	  if (!isMatch) {
+		return res.status(401).json({ message: 'Invalid credentials' });
+	  }
+  
+	  // Generate a JWT token
+	  const token = jwt.sign({ userId: user.userid }, process.env.JWT_SECRET, { expiresIn: '1h' });
+  
+	  // Return the token and user data
+	  return res.status(200).json({
+		message: 'Authentication successful',
+		token,
+		user: {
+		  id: user.userid,
+		  username: user.username,
+		  email: user.useremail,
+		  mobile: user.mobile,
+		  designation: user.designation,
+		  role: user.userrole
+		}
+	  });
+	} catch (error) {
+	  console.error('Error during authentication:', error);
+	  return res.status(500).json({ message: 'Internal server error' });
+	}
+  };
 
-    if (!user) {
-      return res.status(404).json({ message: 'User not found' });
-    }
-
-    // Compare the provided password with the stored hash
-    const isMatch = await bcrypt.compare(password, user.password);
-
-    if (!isMatch) {
-      return res.status(401).json({ message: 'Invalid credentials' });
-    }
-
-    // Generate a JWT token
-    const token = jwt.sign({ userId: user.userid }, process.env.JWT_SECRET, { expiresIn: '1h' });
-
-    return res.status(200).json({ message: 'Authentication successful', token,user });
-  } catch (error) {
-    console.error('Error during authentication:', error);
-    return res.status(500).json({ message: 'Internal server error' });
-  }
-};
-
-module.exports = authenticateUser;
 
 const createUser = async (req, res) => {
 	// Extract user data from the request body
